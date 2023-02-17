412cab6877e90d72d3dd36687fbf38e6a0b31e51
Harshal-Jadhav <harshaljadhav1000@gmail.com>
2023-02-17 23:31:38 +0530

    Project Created working on the data modeling now.

---
+++ PlannerAce/src/test/java/com/plannerAce/PlannerAceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.plannerAce;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PlannerAceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
